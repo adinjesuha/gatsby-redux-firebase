f5306abafc30b710adec7d700d211f6e6e720269
Adin Jesuha <adinjesuha@gmail.com>
2019-10-12 21:32:24 -0600

    .env api key

--- src/service/firebase.js
+++ src/service/firebase.js
@@ -3,13 +3,14 @@ import "firebase/firestore"
 import "firebase/auth"
 
 var config = {
-  apiKey: "AIzaSyCEzlD-Ugrs_8XR6d_N0PZwwUdpGJTVZpE",
+  apiKey: process.env.GATSBY_API_KEY,
   authDomain: "gatsby-firebase-redux.firebaseapp.com",
   databaseURL: "https://gatsby-firebase-redux.firebaseio.com",
   projectId: "gatsby-firebase-redux",
   storageBucket: "gatsby-firebase-redux.appspot.com",
   messagingSenderId: "799298956135",
 }
+
 // Initialize Firebase
 firebase.initializeApp(config)
 // firebase.firestore().settings({ timestampsInSnapshots: true })
